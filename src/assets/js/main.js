183a8ad1845127150846266310409f1d6c1d4a75
Sajjaddi <sajjadjahanids@gmail.com>
2021-11-18 13:28:25 +0330

    add-sale-box-nav-section

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,4 +3,29 @@ $(document).ready(function(){
             $(".toggler i").toggleClass("close-icon")
             $(".menu-navbar nav").toggle("fast")
     });
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        rtl: true,
+        stagePadding: 50,
+        responsive:{
+            0:{
+                items:2
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            },
+            1200:{
+            items:4
+        }
+    }
+    });
+    $('#sale-box-counter').countdown({
+    date:'12/12/2021 23:59:59'
+    });
+    
 })
